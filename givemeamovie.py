f776f9b9a960a2cf50fe39bffb8bc64d179bb7e0
abrahamsk <abrahamskatie@gmail.com>
2016-02-09 14:18:48 -0800

    bad movie

--- givemeamovie.py
+++ givemeamovie.py
@@ -1,4 +1,4 @@
 import random, sys
 movies = ['Wild Wild West', 'The Room', 'Machete', 'Be Cool', 'Iron Sky', 'Mad Max Beyond Thunderdome']
-text = "You're watching..."+(random.choice(movies))+"!\n"
+text = "You're watching... "+(random.choice(movies))+"!\n"
 sys.stdout.write(text)
