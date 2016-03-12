db1734f8550c5d9d55267aae20dd93cd7c84a81f
abrahamsk <abrahamskatie@gmail.com>
2016-03-11 18:06:23 -0800

    list update

--- givemeamovie.py
+++ givemeamovie.py
@@ -9,7 +9,8 @@ movies = {'The Room':'Things spiral out of control in a banker\'s life.\nYou\'re
          'Troll 2':'Vegetarian globlins (no trolls), grandpa\'s ghost, and an ill-fated family trip.', 
          'Masters of the Universe':'Skeletor find the Cosmic Key. He-Man must save the day again!',
          'The Witches':'Boy turns mouse and fights witchez!',
-         'Gymkata':'Main character Cabot must combine his gymnastics skills of the west with fighting secrets of the east and form GYMKATA!'
+         'Gymkata':'Main character Cabot must combine his gymnastics skills of the west with fighting secrets of the east and form GYMKATA!',
+         'Mortal Kombat: Annihilation':'The world was created in six days, so too shall it be destroyed and on the seventh day mankind will rest... in peace.'
          }
 
 peanut_gallery = ['Excellent choice.', 'Oh dear.', 'Grab a drink or three.', 
@@ -34,11 +35,12 @@ commentary = "\n"+(random.choice(peanut_gallery))+"\n"
 sys.stdout.write(result+synopsis+commentary)
 
 # movies we've already subjected ourselves to
-archive = ['A Talking Cat!?!',
-         'Wild Wild West',
-         'Punisher: War Zone',
-         'Piranha 3DD'
-         ]
+archive = {'A Talking Cat!?!':'"The Room" of anthropomorphic animal movies featuring Eric Roberts.  From the AV Club: A Talking Cat?!? is really an exercise in pushing the boundaries of constructive editing far beyond their breaking point.',
+         'Wild Wild West':'Will Smith fights a giant spider in a cowboy hat. Also, naked ladies',
+         'Punisher: War Zone':'Just watch this: https://youtu.be/YF28ceFJJ3A',
+         'Piranha 3DD':'Maddy (Danielle Panabaker), a marine biology student, returns home for the summer to the waterpark she co-owns. She finds to her horror that the other co-owner, her step-father Chet, plans to add an adult-themed section to the waterpark with "water-certified strippers", and re-open it as "Big Wet".  Need I say more?',
+         'Gymkata':'Main character Cabot must combine his gymnastics skills of the west with fighting secrets of the east and form GYMKATA!'
+         }
 
 
 
